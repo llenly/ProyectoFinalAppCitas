084a76910466d68de8a4e7474667cc3f7244cae4
llenly <llenly1985rgmail.com>
2023-04-21 20:48:05 +0200

    permisos para cada vista en el menu

--- resources/views/includes/panel/menu.blade.php
+++ resources/views/includes/panel/menu.blade.php
@@ -30,7 +30,8 @@
                 <i class="ni ni-single-02 text-warning"></i> Clientes
             </a>
         </li>
-    @elseif(auth()->user()->role == 'persontrain')
+        {{-- acceso a la pagina de personal trainer segin el solo los personal trainer  --}}
+    @elseif (auth()->user()->role == 'persontrain')
         <li class="nav-item">
             <a class="nav-link" href="/horario">
                 <i class="ni ni-calendar-grid-58 text-primary"></i> Gestionar Horario
@@ -49,38 +50,55 @@
         </li>
     @else
         <li class="nav-item">
+            <a class="nav-link" href="/horario">
+                <i class="ni ni-calendar-grid-58 text-primary"></i>Reservar Cita
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <i class="fas fa-clock text-info"></i>Mis Citas
+            </a>
+        </li>
+
+        {{-- <li class="nav-item">
             <a class="nav-link" href="">
                 <i class="ni ni-calendar-grid-58 text-primary"></i> Reservar Cita
             </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('logout') }}"
-                onclick="event.preventDefault(); document.getElementById('formLogout').submit();">
-                <i class="fas fa-sign-in-alt"></i> Cerrar Session
-            </a>
+        </li> --}}
+    @endif
 
-            <form action="{{ route('logout') }}" method="POST" style="display: none;" id="formLogout">
-                @csrf
-
-            </form>
-        </li>
-</ul>
-
-<!-- Divider -->
-<hr class="my-3">
-<!-- Heading -->
-<h6 class="navbar-heading text-muted">Reportes</h6>
-<!-- Navigation -->
-<ul class="navbar-nav mb-md-3">
     <li class="nav-item">
-        <a class="nav-link" href="#">
-            <i class="ni ni-books text-default"></i> Citas
+        <a class="nav-link" href="{{ route('logout') }}"
+            onclick="event.preventDefault(); document.getElementById('formLogout').submit();">
+            <i class="fas fa-sign-in-alt"></i> Cerrar Session
         </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="#">
-            <i class="ni ni-chart-bar-32 text-warning"></i> Desempeño Medico Profesional
-        </a>
+
+        <form action="{{ route('logout') }}" method="POST" style="display: none;" id="formLogout">
+            @csrf
+
+        </form>
     </li>
 
-</ul>
+
+
+    @if (auth()->user()->role == 'admin')
+        <!-- Divider -->
+        <hr class="my-3">
+        <!-- Heading -->
+        <h6 class="navbar-heading text-muted">Reportes</h6>
+        <!-- Navigation -->
+        <ul class="navbar-nav mb-md-3">
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="ni ni-books text-default"></i> Citas
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="ni ni-chart-bar-32 text-warning"></i> Desempeño Medico Profesional
+                </a>
+            </li>
+
+        </ul>
+    @endif
